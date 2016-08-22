d47e672f135b5bd8e5b46094b942eda2cce0ecfd
Steffen <grmpfhmbl@gmail.com>
2016-08-22 18:22:37 +0200

    angular2 quickstart + ng2-bootstrap

---
+++ angular2-smart-webgui/README.md
@@ -0,0 +1,9 @@
+# angular2-smart-webgui
+
+## build
+To build this project you need node v4.x.x and npm 3.x.x. To check if
+you're running the correct versions run `node -v` and `npm -v` in a
+terminal/console window.
+
+After checkout run `npm install`. If there is no _typings/_ directory
+created by this command run `npm run typings install`.
---
+++ angular2-smart-webgui/app/app.component.ts
@@ -0,0 +1,47 @@
+/*
+ * copyright (c) 2015 Interfaculty Department of Geoinformatics - Z_GIS (http://www.zgis.at),
+ *                     University of Salzburg (http://www.uni-salzburg.at) in the
+ *                     New Zealand SMART Aquifer Characterisation Programme (http://www.smart-project.info)
+ *                     funded by the Ministry of Business, Innovation and Employment (http://www.mbie.govt.nz/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import {Component} from '@angular/core';
+import {AlertComponent, DATEPICKER_DIRECTIVES} from 'ng2-bootstrap/ng2-bootstrap';
+import {NgModel} from '@angular/forms';
+
+@Component({
+    selector: 'my-app',
+    directives: [AlertComponent, DATEPICKER_DIRECTIVES, NgModel],
+    templateUrl: 'app/app.template.html'
+})
+
+export class AppComponent {
+    public dt:Date = new Date();
+    private minDate:Date = null;
+    private events:Array<any>;
+    private tomorrow:Date;
+    private afterTomorrow:Date;
+    private formats:Array<string> = ['DD-MM-YYYY', 'YYYY/MM/DD', 'DD.MM.YYYY', 'shortDate'];
+    private format = this.formats[0];
+    private dateOptions:any = {
+        formatYear: 'YY',
+        startingDay: 1
+    };
+    private opened:boolean = false;
+
+    public getDate():number {
+        return this.dt && this.dt.getTime() || new Date().getTime();
+    }
+}
