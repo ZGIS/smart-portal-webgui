5ac7e61e7a6eb093406ca56feca672b9ccb55d95
Alex K <akmoch@aut.ac.nz>
2017-01-26 19:01:44 +1300

    removed footer demo, gui still bit sluggish

--- src/app/navigation/footer.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sac-gwh-footer',
-  templateUrl: 'footer.component.html'
-})
-
-export class FooterComponent {
-}
--- src/app/navigation/index.ts
+++ src/app/navigation/index.ts
@@ -1,2 +1,1 @@
 export * from './navigation.component'
-export * from './footer.component'
