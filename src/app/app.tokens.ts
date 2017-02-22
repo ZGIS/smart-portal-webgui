7759d1ebc3f6ea77d755ff2d4bf6869c3a99f82b
Steffen <grmpfhmbl@googlemail.com>
2017-02-22 12:22:17 +0100

    changed CSWI-API urls to match new routes (refs #27)

--- src/app/app.tokens.ts
+++ src/app/app.tokens.ts
@@ -4,7 +4,7 @@ export const CSWI_API_URL = new OpaqueToken('cswiApiUrl');
 export const PORTAL_API_URL = new OpaqueToken('portalApiUrl');
 
 // place an .env, copy example from config/internal/.env.travis
-const envCswiApiUrl = APP_CSWI_API_URL ? APP_CSWI_API_URL : 'https://dev.smart-project.info/cswi-api/query';
+const envCswiApiUrl = APP_CSWI_API_URL ? APP_CSWI_API_URL : 'https://dev.smart-project.info/cswi-api/v1';
 const envPortalApiUrl = APP_PORTAL_API_URL ? APP_PORTAL_API_URL : 'https://dev.smart-project.info/api/v1';
 
 if (!(APP_CSWI_API_URL && APP_PORTAL_API_URL)) {
--- src/app/search/result.service.ts
+++ src/app/search/result.service.ts
@@ -41,7 +41,8 @@ export class ResultService {
       params.set('maxNumberOfResults', maxNumberOfResults.toString());
     }
 
-    return this.http.get(this.cswiApiUrl, {search: params})
+    // TODO SR we should externalize the URL strings to one "service" wrapper class --> cswApiService.getQueryUrl() etc.
+    return this.http.get(this.cswiApiUrl + '/query', {search: params})
     /* FIXME not sure if I'm happy with this so far
      http://stackoverflow.com/questions/22875636/how-do-i-cast-a-json-object-to-a-typescript-class
      */
