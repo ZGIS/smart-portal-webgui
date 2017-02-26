afffcd9998767b9af9460d4e60092197bf0ec49c
Steffen <grmpfhmbl@googlemail.com>
2017-02-26 20:19:52 +0100

    fix linter error introduced by fixing test... #inception

--- src/app/dashboards/card.simple.component.ts
+++ src/app/dashboards/card.simple.component.ts
@@ -16,7 +16,7 @@ export class CardComponent {
   @Output() headerClick = new EventEmitter();
   @Output() readMoreClick = new EventEmitter();
 
-  tempRating: number = 0;
+  tempRating = 0;
 
   headerClicked() {
     this.headerClick.emit();
