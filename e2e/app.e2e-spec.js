f6ad6c6579fc41669a77505f8a7e5a64bd8e40a4
Alexander Kmoch <allixender@gmail.com>
2017-03-23 11:49:52 +1300

    api calls are now included in e2e tests, so as long as they fail in the AOT build (e2e:ci target) mean that the API_URL vars are not properly injected in the AOT build and build fails are exepected (refs #30)

--- e2e/app.e2e-spec.js
+++ e2e/app.e2e-spec.js
@@ -29,21 +29,38 @@ describe('E2E Test Dashboard Maps Category', function () {
 
 });
 
-describe('E2E Test Dashboard Maps Category Query Aquifer', function () {
+describe('E2E Test Dashboard Maps Category Query Aquifer has cards', function () {
 
-  const notExpectedMsg = 'There are no documents for this query / category ';
-
+  const notExpectedMsg = 'There are no documents for this query / category';
 
   beforeEach(function () {
     browser.get('/#/dashboard/1-maps/cards?query=Aquifer');
   });
 
-  it(expectedMsg + ' should have cards', function () {
-    expect(element.all(by.tagName('app-sac-card')).count()).toBeGreaterThanOrEqual(1);
+  it('should not have no-cards-found', function () {
+    expect(element(by.id('no-cards-found')).isPresent()).toBeFalsy();
   });
 
-  it('should not have no-cards-found', function () {
-    expect(element(by.id('no-cards-found')).getText()).toEqual(notExpectedMsg);
+  it('should have cards', function () {
+    expect(element.all(by.tagName('app-sac-card')).count()).toBeGreaterThan(3);
+  });
+
+});
+
+describe('E2E Test Search has list-tems', function () {
+
+  const expectedMsg = 'Search Results - ';
+
+  beforeEach(function () {
+    browser.get('/#/search');
+  });
+
+  it('should contain: ' + expectedMsg, function () {
+    expect(element(by.deepCss('.col-md-4 > div:nth-child(1) > div:nth-child(1)')).getText()).toContain(expectedMsg);
+  });
+
+  it('should have many list items', function () {
+    expect(element.all(by.className('list-group-item')).count()).toBeGreaterThan(3);
   });
 
 });
