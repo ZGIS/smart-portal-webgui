e4dd8046c635e079e3a0b5ffc8561f468a7f432b
Alexander Kmoch <allixender@gmail.com>
2017-03-23 11:09:51 +1300

    testing AOT build in live deploy fails because APP_TOKENS is undefined, which doesn't let the build fail, but will result in failed API calls (refs #30)

--- e2e/app.e2e-spec.js
+++ e2e/app.e2e-spec.js
@@ -8,19 +8,14 @@ describe('E2E Test Welcome Dashboard', function () {
   });
 
   it('should display: ' + expectedMsg, function () {
-    expect(
-      element(
-        by.id('dashboard-home')
-      ).getText()
-    ).toEqual(expectedMsg);
+    expect(element(by.id('dashboard-home')).getText()).toEqual(expectedMsg);
   });
 
 });
 
 describe('E2E Test Dashboard Maps Category', function () {
 
-  const expectedMsg = 'Maps Maps and two-dimensional (map) datasets that are used in groundwater' +
-    ' resource assessments';
+  const expectedMsg = 'Maps Maps and two-dimensional (map) datasets that are used in groundwater' + ' resource assessments';
 
 
   beforeEach(function () {
@@ -29,12 +24,26 @@ describe('E2E Test Dashboard Maps Category', function () {
 
 
   it('should display: ' + expectedMsg, function () {
-    expect(
-      element(
-        by.id('dashboard-category')
-      ).getText()
-    ).toEqual(expectedMsg);
+    expect(element(by.id('dashboard-category')).getText()).toEqual(expectedMsg);
   });
 
 });
 
+describe('E2E Test Dashboard Maps Category Query Aquifer', function () {
+
+  const notExpectedMsg = 'There are no documents for this query / category ';
+
+
+  beforeEach(function () {
+    browser.get('/#/dashboard/1-maps/cards?query=Aquifer');
+  });
+
+  it(expectedMsg + ' should have cards', function () {
+    expect(element.all(by.tagName('app-sac-card')).count()).toBeGreaterThanOrEqual(1);
+  });
+
+  it('should not have no-cards-found', function () {
+    expect(element(by.id('no-cards-found')).getText()).toEqual(notExpectedMsg);
+  });
+
+});
