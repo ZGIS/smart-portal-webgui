ZGIS/smart-portal-webgui

commit 8d8575853c8ffeca875beb168617bd7cd366275c

Opening the modal on cards / search changes URL now to make it bookmarkable (refs #36)

src/app/search/search.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { IGeoFeature, IGeoFeatureCollection, IErrorResult } from './result';
 import { ResultService } from './result.service';
 import * as moment from 'moment';
 import { Ol3MapExtent } from '../ol3-map/ol3-map.component';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { isNullOrUndefined } from 'util';
+import { isNullOrUndefined, isUndefined } from 'util';
 import { NotificationService } from '../notifications/notification.service';
 import { Extent } from 'openlayers';
+import { ResultDetailComponent } from './result-detail.component';
+import { ResultDetailModalComponent } from './result-detail-modal.component';
 
 /**
  * Search Component
@@ -18,6 +20,7 @@ import { Extent } from 'openlayers';
 })
 
 export class SearchComponent implements OnInit {
+  @ViewChild(ResultDetailModalComponent) resultDetailModalComponentRef: ResultDetailModalComponent;
 
   /** initial search */
   search: Search = {
@@ -28,6 +31,8 @@ export class SearchComponent implements OnInit {
     bboxWkt: '',
     maxNumberOfResults: 100
   };
+
+  showModal: String;
 
   /** Search results */
   results: IGeoFeatureCollection;
@@ -67,8 +72,6 @@ export class SearchComponent implements OnInit {
   ngOnInit(): void {
     // parse values from
     this.activatedRoute.queryParams.subscribe((params: Params) => {
-      this.search.query = params['query'] || this.search.query;
-
       if (moment(params['fromDate'], this.DATE_FORMAT).isValid()) {
         this.search.fromDate = moment(params['fromDate'], this.DATE_FORMAT).toDate();
       } else {
@@ -96,7 +99,9 @@ export class SearchComponent implements OnInit {
         this.search.maxNumberOfResults = undefined;
       }
 
-      if (!isNullOrUndefined(params['query'])) {
+      if (!isNullOrUndefined(params['query']) && this.search.query !== params['query']) {
+        this.search.query = params['query'] || this.search.query;
+
         if (this.timeoutId) {
           clearTimeout(this.timeoutId);
         }
@@ -105,6 +110,28 @@ export class SearchComponent implements OnInit {
           this.getResults();
           this.currentUrl = window.location.href;
         }, 250);
+      }
+
+      if (!isNullOrUndefined(params['showModal'])) {
+        this.showModal = params['showModal'];
+        this.resultService.getResults(
+          `fileIdentifier:"${params['showModal']}"`
+        ).subscribe(
+          (results: IGeoFeatureCollection) => {
+            if (results.count > 0) {
+              this.resultDetailModalComponentRef.showFeatureModal(results.features[0]);
+            } else {
+              this.notificationService.addNotification({
+                id: NotificationService.MSG_ID_DOCUMENT_NOT_FOUND,
+                message: `Document ${params['showModal']} could not be found in CSW index. Maybe it is not a metadata document?`,
+                type: NotificationService.NOTIFICATION_TYPE_WARNING
+              });
+              this.resultDetailModalComponentRef.hideFeatureModal();
+            }
+          },
+          (error: IErrorResult) => {
+            this.notificationService.addErrorResultNotification(error);
+          });
       }
     });
   }
@@ -129,7 +156,8 @@ export class SearchComponent implements OnInit {
       fromDate: this.formatDate(this.search.fromDate),
       toDate: this.formatDate(this.search.toDate),
       bbox: JSON.stringify(this.search.bbox),
-      maxNumberOfResults: maxNumberOfResults
+      maxNumberOfResults: maxNumberOfResults,
+      showModal: this.showModal
     };
   }
 
@@ -138,6 +166,7 @@ export class SearchComponent implements OnInit {
    */
   getResults(): void {
     this.isLoading = true;
+
     this.resultService.getResults(
       this.search.query,
       this.formatDate(this.search.fromDate),
@@ -225,8 +254,18 @@ export class SearchComponent implements OnInit {
     if (this.results) {
       return this.results.features
         .filter((item) =>
-          item.properties.title.toLocaleLowerCase().indexOf(this.textFilter.toLocaleLowerCase()) >= 0);
+        item.properties.title.toLocaleLowerCase().indexOf(this.textFilter.toLocaleLowerCase()) >= 0);
     }
+  }
+
+  showFeatureModal(feature: IGeoFeature) {
+    this.showModal = feature.properties.fileIdentifier;
+    this.doSearch();
+  }
+
+  onHideFeatureModal() {
+    this.showModal = undefined;
+    this.doSearch();
   }
 }
 

src/app/app.module.ts
@@ -75,6 +75,7 @@ import { AdminGuard, AuthGuard, RegisteredGuard } from './_guards';
 import { NotificationComponent, NotificationService } from './notifications';
 import { ClipboardModule } from 'ngx-clipboard';
 import { RatingModule } from 'ng2-bootstrap/rating';
+import { ContextRetrieveComponent } from './context/context.component';
 
 export function cookieServiceFactory() {
   return new CookieService();
@@ -136,7 +137,8 @@ export function cookieServiceFactory() {
     OwcDocumentPropertiesComponent,
     OwcEntryPropertiesComponent,
     BasicFileUploadComponent,
-    CardComponent
+    CardComponent,
+    ContextRetrieveComponent
   ],
   providers: [
     API_URL_PROVIDERS,

src/app/app.routing.ts
@@ -22,6 +22,7 @@ import { NotFoundComponent } from './navigation';
 import { ResearchProgrammesComponent } from './research-pg';
 import { GlossaryComponent } from './glossary-edu';
 import { AdminComponent } from './admin';
+import { ContextRetrieveComponent } from './context/context.component';
 
 const appRoutes: Routes = [
   {
@@ -78,6 +79,12 @@ const appRoutes: Routes = [
     path: 'x-admin',
     component: AdminComponent,
     canActivate: [AdminGuard]
+  },
+
+  // to access content directly
+  {
+    path: 'context/:type/:uuid',
+    component: ContextRetrieveComponent
   },
   // also think about CanActivateChild for the whole workbench thing
   {

src/app/context/context.component.ts
@@ -1,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NotificationService } from '../notifications/notification.service';
+
+/**
+ * This component parses the context URL and forwards to the appropriate page
+ */
+@Component({
+  selector: 'app-sac-gwh-context-retrieve',
+  template: '<i class="fa fa-spinner fa-pulse fa-fw"></i> retrieving content and redirecting...'
+})
+
+export class ContextRetrieveComponent implements OnInit {
+
+  private TYPE_ENTRY = 'entry';
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private notificationService: NotificationService) {
+  }
+
+  ngOnInit(): void {
+    this.route.params
+      .subscribe((params: Params) => {
+        switch (params['type']) {
+          case this.TYPE_ENTRY: {
+            setTimeout(() =>
+            this.router.navigate(['search/'], { queryParams: {
+              query: `fileIdentifier:"${params['uuid']}"`,
+              showModal: params['uuid']
+            }})
+            );
+            break;
+          }
+          default: {
+            this.notificationService.addNotification({
+              message: `Unknown context ${params['type']}`,
+              type: 'danger',
+              id: NotificationService.MSG_ID_ERROR});
+            this.router.navigate(['']);
+          }
+        }
+      });
+  }
+}
+

src/app/context/index.ts
@@ -1,0 +1,1 @@
+export * from './context.component'

src/app/dashboards/result-cards.component.html
@@ -40,8 +40,8 @@
 
   <div *ngFor="let feature of (getFilteredResults(origin))" class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
 
-    <app-sac-card (headerClick)="resultCardDetailRef.showFeatureModal(feature)"
-                  (readMoreClick)="resultCardDetailRef.showFeatureModal(feature)"
+    <app-sac-card (headerClick)="showFeatureModal(feature)"
+                  (readMoreClick)="showFeatureModal(feature)"
                   cardImage='/images/stylish-folder.jpg'
                   [bodyText]="feature.properties.abstrakt"
                   [title]="feature.properties.title"
@@ -98,5 +98,5 @@
   </div>
 </div>
 
-<app-sac-gwh-result-detail-modal #resultCardDetailRef></app-sac-gwh-result-detail-modal>
+<app-sac-gwh-result-detail-modal (onHide)="onHideFeatureModal()"></app-sac-gwh-result-detail-modal>
 

src/app/dashboards/result-cards.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { IGeoFeatureCollection, IGeoFeature, ResultService } from '../search';
 import * as moment from 'moment';
 import { NotificationService } from '../notifications/notification.service';
 import { CategoriesService } from './categories.service';
+import { ResultDetailModalComponent } from '../search/result-detail-modal.component';
+import { isNullOrUndefined } from 'util';
+import { IErrorResult } from '../search/result';
 
 // let moment = require('moment');
 
@@ -21,15 +24,20 @@ import { CategoriesService } from './categories.service';
  * CardComponent to show search results in Dashboard
  */
 export class ResultCardsComponent implements OnInit, OnDestroy {
+  @ViewChild(ResultDetailModalComponent) resultDetailModalComponentRef: ResultDetailModalComponent;
 
   results: IGeoFeatureCollection;
   resultsGroups: String[];
 
   categoryName = '';
+  categoryId = '';
+  query = '';
   description = '';
   loading = true;
 
   textFilter = '';
+
+  showModal: String;
 
   private subscription: Subscription;
 
@@ -38,11 +46,13 @@ export class ResultCardsComponent implements OnInit, OnDestroy {
    * @param resultService       - injected ResultService
    * @param activatedRoute      - injected ActivatedRoute
    * @param notificationService - injected NotificationService
+   * @param router              - injected Router
    */
   constructor(private resultService: ResultService,
               private activatedRoute: ActivatedRoute,
               private categoriesService: CategoriesService,
-              private notificationService: NotificationService) {
+              private notificationService: NotificationService,
+              private router: Router) {
   }
 
   /**
@@ -60,9 +70,9 @@ export class ResultCardsComponent implements OnInit, OnDestroy {
 
     // subscribe to router event
     this.subscription = this.activatedRoute.queryParams.subscribe(
-      (param: any) => {
-        let categoryId = param['categoryId'];
-        this.categoryName = categoryId;
+      (params: Params) => {
+        let categoryId = params['categoryId'];
+        this.categoryId = categoryId;
         this.categoriesService.getCildCategoryById(categoryId)
           .subscribe(
             catObj => {
@@ -78,30 +88,51 @@ export class ResultCardsComponent implements OnInit, OnDestroy {
               this.notificationService.addErrorResultNotification(error);
             });
 
-        let query = param['query'];
+        if (params['query'] !== this.query) {
+          this.query = params['query'] || '*:*';
 
-        if (!query) {
-          query = '*:*';
+          this.resultService.getResults(
+            this.query,
+            '1970-01-01',
+            moment().format('YYYY-MM-DD'),
+            // 'ENVELOPE(147.7369328125,201.7896671875,-23.1815078125,-50.5154921875)'
+            'ENVELOPE(-180,180,-90,90)'
+          ).subscribe(
+            (results: IGeoFeatureCollection) => {
+              this.loading = false;
+              this.results = results;
+              // this.resultsGroups = this.getCataloguesOfResults();
+              this.resultsGroups = ['Best results', 'Journal Articles', 'Other results'];
+            },
+            (error: any) => {
+              this.loading = false;
+              this.notificationService.addErrorResultNotification(error);
+            }
+          );
         }
 
-        this.resultService.getResults(
-          query,
-          '1970-01-01',
-          moment().format('YYYY-MM-DD'),
-          // 'ENVELOPE(147.7369328125,201.7896671875,-23.1815078125,-50.5154921875)'
-          'ENVELOPE(-180,180,-90,90)'
-        ).subscribe(
-          (results: IGeoFeatureCollection) => {
-            this.loading = false;
-            this.results = results;
-            // this.resultsGroups = this.getCataloguesOfResults();
-            this.resultsGroups = ['Best results', 'Journal Articles', 'Other results'];
-          },
-          (error: any) => {
-            this.loading = false;
-            this.notificationService.addErrorResultNotification(error);
-          }
-        );
+        if (!isNullOrUndefined(params['showModal'])) {
+          this.showModal = params['showModal'];
+          this.resultService.getResults(
+            `fileIdentifier:"${params['showModal']}"`
+          ).subscribe(
+            (results: IGeoFeatureCollection) => {
+              if (results.count > 0) {
+                this.resultDetailModalComponentRef.showFeatureModal(results.features[0]);
+              } else {
+                this.notificationService.addNotification({
+                  id: NotificationService.MSG_ID_DOCUMENT_NOT_FOUND,
+                  message: `Document ${params['showModal']} could not be found in CSW index. Maybe it is not a metadata document?`,
+                  type: NotificationService.NOTIFICATION_TYPE_WARNING
+                });
+                this.resultDetailModalComponentRef.hideFeatureModal();
+              }
+            },
+            (error: IErrorResult) => {
+              this.notificationService.addErrorResultNotification(error);
+            });
+        }
+
       });
   }
 
@@ -134,6 +165,30 @@ export class ResultCardsComponent implements OnInit, OnDestroy {
       return filteredByOrigin.filter((item) =>
       item.properties.title.toLocaleLowerCase().indexOf(this.textFilter.toLocaleLowerCase()) >= 0);
     }
+  }
+
+  showFeatureModal(feature: IGeoFeature) {
+    this.showModal = feature.properties.fileIdentifier;
+    console.log(this.activatedRoute.snapshot);
+    console.log();
+    this.router.navigate([this.activatedRoute.snapshot.url.join('/')], {
+      queryParams: {
+        query: this.query,
+        categoryId: this.categoryId,
+        showModal: this.showModal
+      }
+    });
+  }
+
+  onHideFeatureModal() {
+    this.showModal = undefined;
+    this.router.navigate([this.activatedRoute.snapshot.url.join('/')], {
+      queryParams: {
+        query: this.query,
+        categoryId: this.categoryId,
+        showModal: this.showModal
+      }
+    });
   }
 
   /**

src/app/notifications/notification.service.ts
@@ -46,11 +46,27 @@ export class NotificationService {
   /** Message that was created by file uploader */
   public static MSG_ID_FILE_UPLOADER = 5;
 
+  /** Message that shows the "document not found" when using "showModal" for example on search results. */
+  public static MSG_ID_DOCUMENT_NOT_FOUND = 6;
+
   /** default dismiss time in milliseconds */
   public static DEFAULT_DISMISS = 5000;
 
   /** constant to indicate that the notification should not be automatically dismissed after some time */
   public static DISMISS_NEVER = -1;
+
+  /** constant for notification type to indicate a successful operation */
+  public static NOTIFICATION_TYPE_SUCCESS = 'success';
+
+  /** constant for notification type to show a information message */
+  public static NOTIFICATION_TYPE_INFO = 'info';
+
+  /** constant for notification type to show a warning */
+  public static NOTIFICATION_TYPE_WARNING = 'warning';
+
+  /** constant for notification type to show an error */
+  public static NOTIFICATION_TYPE_DANGER = 'danger';
+
 
   /** array storing all displayed notifications */
   notifications: SacGwhNotification[] = [];

commit 5a17e225b3c2660cd7699fcece3e51b67f94e6ee

fix search bug introduced by 8d85758 (build/test failure)

src/app/search/search.component.ts
@@ -99,7 +99,7 @@ export class SearchComponent implements OnInit {
         this.search.maxNumberOfResults = undefined;
       }
 
-      if (!isNullOrUndefined(params['query']) && this.search.query !== params['query']) {
+      if (this.search.query !== params['query']) {
         this.search.query = params['query'] || this.search.query;
 
         if (this.timeoutId) {