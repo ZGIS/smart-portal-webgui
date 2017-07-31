2ec2954394fc4b7fe7b5665f1b7548f481658dd9
Alexander Kmoch <allixender@gmail.com>
2017-07-31 17:55:47 +0300

    owc resource in details modal(#63)

--- src/app/search/result-detail-modal.component.ts
+++ src/app/search/result-detail-modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap/modal';
-import { IGeoFeature } from './result';
+import { IErrorResult, IGeoFeature } from './result';
+import { OwcContext, OwcResource } from '../owc/collections';
+import { ResultService } from './result.service';
+import { NotificationService } from '../notifications/notification.service';
 
 @Component({
   selector: 'app-sac-gwh-result-detail-modal',
@@ -9,14 +12,40 @@ import { IGeoFeature } from './result';
 
 export class ResultDetailModalComponent {
   feature: IGeoFeature;
+  owcFeature: OwcResource;
 
   @Output() onHide = new EventEmitter();
 
   @ViewChild('resultModalRef') public modal: ModalDirective;
 
-  showFeatureModal(geoFeature: IGeoFeature) {
+  constructor( private resultService: ResultService,
+               private notificationService: NotificationService ) {
+  }
+
+  showFeatureModal( geoFeature: IGeoFeature ) {
     if (geoFeature !== undefined) {
       this.feature = geoFeature;
+
+      this.resultService.getResultsAsOwcGeoJson(
+        `fileIdentifier:"${geoFeature.properties.fileIdentifier}"`
+      ).subscribe(
+        ( results: OwcContext ) => {
+          if (results.features.length > 0) {
+            this.owcFeature = results.features[ 0 ];
+          } else {
+            this.notificationService.addNotification({
+              id: NotificationService.MSG_ID_DOCUMENT_NOT_FOUND,
+              message: `Document ${geoFeature.properties.fileIdentifier}
+              could not be found in CSW index. Maybe it is not a metadata document?`,
+              type: NotificationService.NOTIFICATION_TYPE_WARNING
+            });
+            this.hideFeatureModal();
+          }
+        },
+        ( error: IErrorResult ) => {
+          this.notificationService.addErrorResultNotification(error);
+        });
+
       this.modal.show();
     }
   }
