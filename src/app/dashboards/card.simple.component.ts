ZGIS/smart-portal-webgui

commit 59c064ec15b41a042f6130c9d4a0fe2c1e850b08

clarified rating value of result cards

src/app/dashboards/card.simple.component.ts
@@ -20,6 +20,21 @@ export class CardComponent {
   @Output() readMoreClick = new EventEmitter();
 
 
+  getProgressLabel(value: number): string {
+    if (value < 0.1) {
+      return '<10%';
+    } else {
+      return `${this.getProgressValue(value)}%`;
+    }
+  }
+
+  getProgressValue(value: number): string {
+    if (value < 0.1) {
+      return '10';
+    } else {
+      return (value * 100).toFixed(0);
+    }
+  }
 
   headerClicked() {
     this.headerClick.emit();
@@ -33,4 +48,15 @@ export class CardComponent {
     return str.split(/\W/, number).join(' ');
   }
 
+  private getProgressType(value: number): string {
+    if (value > 0.75) {
+      return 'success';
+    } else if (value > 0.5) {
+      return 'info';
+    } else if (value > 0.1) {
+      return 'warning';
+    } else {
+      return 'default';
+    }
+  }
 }

src/app/dashboards/card.simple.component.html
@@ -9,23 +9,12 @@
 
   </div>
   <div class="card-content">
-
-    <progressbar class="rating" *ngIf="tempRating && tempRating > 0.75" placement="bottom"
-                 tooltip="Estimated  relative relevance: {{ (tempRating * 100).toFixed(0) }}%"
-                 [animate]="false" type="success" [value]="(tempRating * 100).toFixed(0)">
-      <b>{{ (tempRating * 100).toFixed(0) }}%</b>
-    </progressbar>
-
-    <progressbar class="rating" *ngIf="tempRating && tempRating > 0.5 && tempRating < 0.75" placement="bottom"
-                 tooltip="Estimated  relative relevance: {{ (tempRating * 100).toFixed(0) }}%"
-                 [animate]="false" type="info" [value]="(tempRating * 100).toFixed(0)">
-      <b>{{ (tempRating * 100).toFixed(0) }}%</b>
-    </progressbar>
-
-    <progressbar class="rating" *ngIf="tempRating && tempRating > 0.1 && tempRating < 0.5" placement="bottom"
-                 tooltip="Estimated  relative relevance: {{ (tempRating * 100).toFixed(0) }}%"
-                 [animate]="false" type="warning" [value]="(tempRating * 100).toFixed(0)">
-      <b>{{ (tempRating * 100).toFixed(0) }}%</b>
+    <progressbar class="rating" placement="bottom"
+                 tooltip="Estimated relevance of this result compared to the top result. The top result is always 100%."
+                 [animate]="false"
+                 [type]="getProgressType(tempRating)"
+                 [value]="getProgressValue(tempRating)">
+      <b>{{getProgressLabel(tempRating)}}</b>
     </progressbar>
     <div>
       {{ getFirstWords(bodyText, 100)}}

src/app/dashboards/result-cards.component.html
@@ -18,6 +18,8 @@
         <i class="fa fa-arrow-left" aria-hidden="true"></i>&nbsp;back</a>
       </div>
       <div class="panel-body">
+        <alert type="info">The shown relevance value is relative to the top document. The best fitting document
+        always gets a value of 100%. Every other document is rated relative to this document.</alert>
         <div class="list-group">
           <div class="list-group-item">
             Description and keywords for this category: {{ description }}

commit 8429a88a20df85a7f1424e99b6d7bb336795fdf6

build error fixed introduced with 59c064e
See https://travis-ci.org/ZGIS/smart-portal-webgui/builds/270882093

src/app/dashboards/card.simple.component.ts
@@ -36,6 +36,18 @@ export class CardComponent {
     }
   }
 
+  getProgressType(value: number): string {
+    if (value > 0.75) {
+      return 'success';
+    } else if (value > 0.5) {
+      return 'info';
+    } else if (value > 0.1) {
+      return 'warning';
+    } else {
+      return 'default';
+    }
+  }
+
   headerClicked() {
     this.headerClick.emit();
   }
@@ -48,15 +60,4 @@ export class CardComponent {
     return str.split(/\W/, number).join(' ');
   }
 
-  private getProgressType(value: number): string {
-    if (value > 0.75) {
-      return 'success';
-    } else if (value > 0.5) {
-      return 'info';
-    } else if (value > 0.1) {
-      return 'warning';
-    } else {
-      return 'default';
-    }
-  }
 }