2d382d54b3946ba578145be5a5311c76d425e508
Alexander Kmoch <allixender@gmail.com>
2017-12-04 12:14:48 +0200

    share buttons now use result card title and url #26

--- src/app/dashboards/share-button.component.ts
+++ src/app/dashboards/share-button.component.ts
@@ -1,8 +1,9 @@
 import {
   Component,
   AfterViewInit,
+  OnInit,
   ViewChild,
-  ElementRef
+  ElementRef, Input
 } from '@angular/core';
 import { ShareButtonService } from './share-button.service';
 
@@ -11,7 +12,8 @@ import { ShareButtonService } from './share-button.service';
  */
 @Component({
   selector: 'app-share-buttons',
-  template: '<div #shareThis class="a2a_kit a2a_kit_size_32 a2a_default_style">' +
+  template: '<div #shareThis class="a2a_kit a2a_kit_size_32 a2a_default_style"' +
+  ' [attr.data-a2a-url]="dataA2aUrlExt" [attr.data-a2a-title]="dataA2aTitle">' +
   '  <a class="a2a_dd" href="https://www.addtoany.com/share"></a>' +
   '  <a class="a2a_button_facebook"></a>' +
   '  <a class="a2a_button_twitter"></a>' +
@@ -21,15 +23,27 @@ import { ShareButtonService } from './share-button.service';
   '</div>'
 })
 
-export class ShareButtonComponent implements AfterViewInit {
+export class ShareButtonComponent implements AfterViewInit, OnInit {
   @ViewChild('shareThis') targetRef: ElementRef;
+  // data-a2a-url="http://www.example.com/page.html" data-a2a-title="Example Page Title"
+  @Input() dataA2aTitle: string;
+  @Input() dataA2aUrl: string;
+  dataA2aUrlExt = 'https://dev.smart-portal.info/#/context/resource/';
 
-  constructor(private shareButtonService: ShareButtonService) {
+  constructor( private shareButtonService: ShareButtonService ) {
+    if (this.dataA2aUrl) {
+      this.dataA2aUrlExt = this.dataA2aUrlExt + this.dataA2aUrl;
+    }
   }
 
+  ngOnInit(): void {
+    if (this.dataA2aUrl) {
+      this.dataA2aUrlExt = this.dataA2aUrlExt + this.dataA2aUrl;
+    }
+  }
   ngAfterViewInit() {
     this.shareButtonService.getReady().subscribe(
-      (ready) => {
+      ( ready ) => {
         if (!ready) {
           return;
         }
