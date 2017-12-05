1819a1977196a83ed9f4f18a1dac07fd3d6f819b
Alexander Kmoch <allixender@gmail.com>
2017-12-05 21:13:31 +0200

    build issues, testing cypress alternative to protractor

--- cypress/integration/gwhub_cy_spec.js
+++ cypress/integration/gwhub_cy_spec.js
@@ -13,20 +13,6 @@ describe('E2E Cypress Gw Hub Test', function () {
     });
   });
 
-  context('E2E Test Dashboard Category', function () {
-
-    // const expectedMsg = 'Maps Maps and two-dimensional (map) datasets that are used in groundwater' + ' resource assessments';
-    const expectedMsg = 'Understand our groundwater systems';
-
-    beforeEach(function () {
-      cy.visit('http://localhost:8080/#/dashboard/1-understanding?categoryId=1');
-    });
-
-    it('should display: ' + expectedMsg, function () {
-      cy.get('#dashboard-category').should('contain', expectedMsg);
-    });
-  });
-
   context('E2E Test Dashboard Category has cards', function () {
 
     const notExpectedMsg = 'There are no documents for this query / category';
@@ -39,25 +25,6 @@ describe('E2E Cypress Gw Hub Test', function () {
       // expect(element(by.id('no-cards-found')).isPresent()).toBeFalsy();
       // cy.get('.col-md-12').find('#no-cards-found').should('have.length', 0);
       cy.get('#no-cards-found').should('not.exist');
-    });
-
-  });
-
-  context('E2E Test Dashboard Category has more cards', function () {
-
-    const notExpectedMsg = 'There are no documents for this query / category';
-
-    beforeEach(function () {
-      cy.visit('http://localhost:8080/#/dashboard/7-protect/cards?query=(keywords%3A%22nz%20water%20quality%20maps%22)%5E1.5%20OR%20catch_all%3Anz%20water%20quality%20maps&categoryId=70');
-    });
-
-    it('should have cards', function () {
-
-      // expect(element.all(by.tagName('app-sac-card')).count()).toBeGreaterThan(3);
-      // body > app-sac-gwh > div > app-sac-gwh-result-cards > div:nth-child(2) > div:nth-child(2) > app-sac-card
-      // cy.get('.row').find('app-sac-card').should('have.length', 'above', 3);
-      // cy.get('app-sac-card').should('have.length', 'above', 3);
-      cy.get('app-sac-gwh-result-cards > div.row > div.col-md-4').find('app-sac-card').its('length').should('be.gt', 3);
     });
 
   });
@@ -81,7 +48,6 @@ describe('E2E Cypress Gw Hub Test', function () {
       // cy.get('.list-group').find('.list-group-item').should('have.length', 'greater.than', 3)
       cy.get('div.col-md-4 > div.panel > div.panel-body > div > div.list-group').find('a.list-group-item').its('length').should('be.gt', 3);
     });
-
   });
 
 });
---
+++ cypress/integration/local_cy_spec.js
@@ -0,0 +1,38 @@
+// https://docs.cypress.io/guides/references/assertions.html#BDD-Assertions
+
+describe('E2E Cypress Gw Hub Local Tests that timeout on Travis', function () {
+
+  context('E2E Test Dashboard Category', function () {
+
+    // const expectedMsg = 'Maps Maps and two-dimensional (map) datasets that are used in groundwater' + ' resource assessments';
+    const expectedMsg = 'Understand our groundwater systems';
+
+    beforeEach(function () {
+      cy.visit('http://localhost:8080/#/dashboard/1-understanding?categoryId=1');
+    });
+
+    it('should display: ' + expectedMsg, function () {
+      cy.get('#dashboard-category').should('contain', expectedMsg);
+    });
+  });
+
+  context('E2E Test Dashboard Category has more cards', function () {
+
+    const notExpectedMsg = 'There are no documents for this query / category';
+
+    beforeEach(function () {
+      cy.visit('http://localhost:8080/#/dashboard/7-protect/cards?query=(keywords%3A%22nz%20water%20quality%20maps%22)%5E1.5%20OR%20catch_all%3Anz%20water%20quality%20maps&categoryId=70');
+    });
+
+    it('should have cards', function () {
+
+      // expect(element.all(by.tagName('app-sac-card')).count()).toBeGreaterThan(3);
+      // body > app-sac-gwh > div > app-sac-gwh-result-cards > div:nth-child(2) > div:nth-child(2) > app-sac-card
+      // cy.get('.row').find('app-sac-card').should('have.length', 'above', 3);
+      // cy.get('app-sac-card').should('have.length', 'above', 3);
+      cy.get('app-sac-gwh-result-cards > div.row > div.col-md-4').find('app-sac-card').its('length').should('be.gt', 3);
+    });
+
+  });
+
+});
