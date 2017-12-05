ad0bbc66ebfa438c4cabbe052bac4b93cbc57662
Alexander Kmoch <allixender@gmail.com>
2017-12-05 15:27:49 +0200

    build issues

--- e2e/app.e2e-spec.js
+++ e2e/app.e2e-spec.js
@@ -31,11 +31,11 @@ describe('E2E Test Dashboard Category', function () {
   it('should display: ' + expectedMsg, function () {
     //SR the category description has more content than just the description
     //SR changed toEqual -> toContain
-    browser.refresh();
-    browser.waitForAngular();
+    // browser.refresh();
+    // browser.waitForAngular();
     browser.wait(function() {
       return element(by.id('dashboard-category')).isPresent();
-    }, 10000);
+    }, 30000);
     expect(element(by.id('dashboard-category')).getText()).toContain(expectedMsg);
   });
 
@@ -64,21 +64,21 @@ describe('E2E Test Dashboard Category has more cards', function () {
   const notExpectedMsg = 'There are no documents for this query / category';
 
   beforeEach(function () {
-    browser.ignoreSynchronization = true;
+    // browser.ignoreSynchronization = true;
     browser.get('/#/dashboard/7-protect/cards?query=(keywords%3A%22nz%20water%20quality%20maps%22)%5E1.5%20OR%20catch_all%3Anz%20water%20quality%20maps&categoryId=70');
   });
 
-  // FIXME fails now consistently for no reason (on Travis, but successful locally)
+  // FIXME fails consistently for no reason (on Travis, but successful locally)
   it('should have cards', function () {
     browser.refresh();
     browser.wait(function() {
       return element(by.id('dashboard-category')).isPresent();
-    }, 10000);
+    }, 30000);
     expect(element.all(by.tagName('app-sac-card')).count()).toBeGreaterThan(3);
   });
 
   afterEach(function () {
-    browser.ignoreSynchronization = false;
+    // browser.ignoreSynchronization = false;
   });
 
 });
