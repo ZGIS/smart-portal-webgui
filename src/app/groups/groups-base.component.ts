1f8a3c348bc44e8aa9dded0920a73c010a6663ee
Alexander Kmoch <allixender@gmail.com>
2018-02-20 18:42:32 +0200

    users and groups WIP #60

--- src/app/groups/groups-base.component.ts
+++ src/app/groups/groups-base.component.ts
@@ -192,7 +192,7 @@ export class GroupsBaseComponent implements OnInit {
     }
   }
 
-  deleteGroup(groupId: string): void {
+  deleteGroup( groupId: string ): void {
     console.log('we delete the group');
     this.workbenchService.deleteUsersOwnUserGroup(groupId).subscribe(
       deleted => {
@@ -209,6 +209,75 @@ export class GroupsBaseComponent implements OnInit {
         console.log(<any>error);
         this.notificationService.addErrorResultNotification(error);
       });
+  }
+
+  isValidEmail( email: string ): boolean {
+    console.log(email + ' ' + email.trim().length);
+    // and pattern for email?
+    if (email) {
+      return email.trim().length > 0;
+    } else {
+      return false;
+    }
+  }
+
+  addUserByEmail( myself: string ): void {
+    if (this.isValidEmail(this.emailUserToAdd)) {
+      this.loading = true;
+      this.workbenchService.findUserForEmail(this.emailUserToAdd).subscribe(
+        found => {
+          if (this.editUserGroup.hasUsersLevel.findIndex(p => p.users_accountsubject === found.accountSubject) < 0 &&
+            found.accountSubject !== myself) {
+            let newGroupUserLevel: UserGroupUsersLevel = <UserGroupUsersLevel>{
+              usergroups_uuid: this.editUserGroup.uuid,
+              users_accountsubject: found.accountSubject,
+              userlevel: 0
+            };
+            this.editUserGroup.hasUsersLevel.push(newGroupUserLevel);
+            this.workbenchService.updateUsersOwnUserGroup(this.editUserGroup).subscribe(
+              success => {
+                this.notificationService.addNotification({
+                  id: NotificationService.DEFAULT_DISMISS,
+                  type: 'info',
+                  message: `The user has been added, reloading.`
+                });
+                this.readyForAddingConllection = false;
+                this.emailUserToAdd = '';
+                this.loading = false;
+                this.hideEditGroupModal();
+                this.reloadGroups();
+              },
+              error => {
+                this.loading = false;
+                console.log(<any>error);
+                this.notificationService.addErrorResultNotification(error);
+              });
+          } else {
+            this.loading = false;
+            this.notificationService.addNotification({
+              id: NotificationService.DEFAULT_DISMISS,
+              type: 'warning',
+              message: `User to add is already a member.`
+            });
+          }
+        },
+        error => {
+          this.loading = false;
+          console.log(<any>error);
+          this.notificationService.addNotification({
+            id: NotificationService.DEFAULT_DISMISS,
+            type: 'warning',
+            message: `User couldn not be found.`
+          });
+        });
+    } else {
+      this.loading = false;
+      this.notificationService.addNotification({
+        id: NotificationService.DEFAULT_DISMISS,
+        type: 'warning',
+        message: `Not a valid user email to add.`
+      });
+    }
   }
 
   getContextVisibilityForNumber( level: number ): string {
