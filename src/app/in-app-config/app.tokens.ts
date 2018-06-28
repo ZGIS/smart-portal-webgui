1bfd50e537cd442fe198497c25172b930600a2c8
Alexander Kmoch <allixender@gmail.com>
2018-06-28 19:20:09 +0300

    app tokens

--- src/app/in-app-config/app.tokens.ts
+++ src/app/in-app-config/app.tokens.ts
@@ -2,8 +2,8 @@ import { OpaqueToken } from '@angular/core';
 
 // place an .env, copy example from config/internal/.env.travis
 // this works in dev test and prod jit, used to fail silently in prod aot, but seems to work now reliably
-const envCswiApiUrl: string = APP_CSWI_API_URL ? APP_CSWI_API_URL : 'https://dev.smart-project.info/cswi-api/v1';
-const envPortalApiUrl: string = APP_PORTAL_API_URL ? APP_PORTAL_API_URL : 'https://dev.smart-project.info/api/v1';
+const envCswiApiUrl: string = APP_CSWI_API_URL ? APP_CSWI_API_URL : 'https://foodev.smart-project.info/cswi-api/v1';
+const envPortalApiUrl: string = APP_PORTAL_API_URL ? APP_PORTAL_API_URL : 'https://foodev.smart-project.info/api/v1';
 let envAppVersion: string = APP_VERSION ? APP_VERSION : 'Snapshot';
 
 let currDate = new Date();
@@ -21,6 +21,8 @@ export const CSWI_API_URL = new OpaqueToken('cswiApiUrl');
 export const PORTAL_API_URL = new OpaqueToken('portalApiUrl');
 export const WEBGUI_APP_VERSION = new OpaqueToken('appVersion');
 
+console.log(PORTAL_API_URL);
+
 export const API_URL_PROVIDERS = [
   {
     provide: CSWI_API_URL,
@@ -31,6 +33,8 @@ export const API_URL_PROVIDERS = [
     useValue: envPortalApiUrl
   }
 ];
+
+console.log(PORTAL_API_URL);
 
 export const APP_VERSION_PROVIDERS = [
   {
